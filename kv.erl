f82bdbcb0509a0b93b8a704cedf97d839be2bfc3
Sönke Hahn <soenkehahn@gmail.com>
2014-09-05 16:35:28 +0200

    foo

--- kv.erl
+++ kv.erl
@@ -29,13 +29,13 @@ server(T) ->
 
 insert(K,V,leaf) ->
   {node,leaf,K,V,leaf};
-insert(K,V,{node,L,KN,_VN,R}) ->
+insert(K,V,{node,L,KN,VN,R}) ->
   if K<KN ->
-      insert(K,V,L);
+      {node,insert(K,V,L),KN,VN,R};
      K==KN ->
       {node,L,K,V,R};
      K>KN ->
-      insert(K,V,R)
+      {node,L,KN,VN,insert(K,V,R)}
   end.
 
 lookup(_,leaf) ->
